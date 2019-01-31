98ce32df34b8522b4f9925e3b2d579d0ed2eae83
Mohammed Ehab <mohammed.ehab@sap.com>
2019-01-31 21:46:22 +0530

    Added a describe() to test case

--- contracts/test/curator.js
+++ contracts/test/curator.js
@@ -1,28 +1,29 @@
 var Curator = artifacts.require("Curator");
 
 contract("Curator", function(accounts){
-
-    it("Should succesfully perform a VOUCH vote", function(){
-        return Curator.deployed().then(function(instance){
-            return instance.vouchOrReject.call("t1", true, {from: accounts[0]});
-        }).then(function(voteSuccessful){
-            assert.equal(voteSuccessful, true, "Voting is successful");
+    describe("Curator Interaction", function(){
+        it("Should succesfully perform a VOUCH vote", function(){
+            return Curator.deployed().then(function(instance){
+                return instance.vouchOrReject.call("t1", true, {from: accounts[0]});
+            }).then(function(voteSuccessful){
+                assert.equal(voteSuccessful, true, "Voting is successful");
+            });
         });
-    });
-
-    it("Should succesfully perform a REJECT vote", function(){
-        return Curator.deployed().then(function(instance){
-            return instance.vouchOrReject.call("t1", false, {from: accounts[1]});
-        }).then(function(voteSuccessful){
-            assert.equal(voteSuccessful, true, "Voting is successful");
+    
+        it("Should succesfully perform a REJECT vote", function(){
+            return Curator.deployed().then(function(instance){
+                return instance.vouchOrReject.call("t1", false, {from: accounts[1]});
+            }).then(function(voteSuccessful){
+                assert.equal(voteSuccessful, true, "Voting is successful");
+            });
         });
-    });
-
-    it("Should reset all available user votes", function(){
-        return Curator.deployed().then(function(instance){
-            return instance.resetVoteCount.call();
-        }).then(function(isDone){
-            assert.equal(isDone, true,"Votes reset!");
+    
+        it("Should reset all available user votes", function(){
+            return Curator.deployed().then(function(instance){
+                return instance.resetVoteCount.call();
+            }).then(function(isDone){
+                assert.equal(isDone, true,"Votes reset!");
+            });
         });
-    });
+    })
 })
